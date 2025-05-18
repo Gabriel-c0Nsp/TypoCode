cdcd9a95859b471b9886797c2c32a600a396cbdc
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2025-05-18 18:13:17 -0300

    removed list and added vector buffer structure

--- src/typo.c
+++ src/typo.c
@@ -1,29 +1,23 @@
 #define NCURSES_WIDECHAR 1
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <curses.h>
 #include <locale.h>
 #include <ncurses.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <wchar.h>
 
-// Doubly linked list to store file buffer
-typedef struct BF_Node {
-  wchar_t file_char;
-  struct BF_Node *next;
-  struct BF_Node *prev;
-} BF_Node;
-
-BF_Node *create_node(wchar_t file_char);
-void insert_buffer(BF_Node **buffer, wchar_t file_char);
-void clean_buffer(BF_Node **buffer);
+typedef struct Buffer {
+  int current_cu_pointer;
+  wchar_t *vect_buff;
+} Buffer;
 
 FILE *open_file(char *argv);
 void close_file(FILE *file_path);
 
-void store_file_buffer(BF_Node **buffer, FILE *file);
+void store_file_buffer(Buffer buffer, FILE file_path);
 
-void display_buffer(BF_Node *buffer);
+void display_buffer(Buffer buffer);
 
 int y_cursor_pos = 0;
 int x_cursor_pos = 0;
@@ -31,12 +25,9 @@ int x_cursor_pos = 0;
 int main(int argc, char *argv[]) {
   setlocale(LC_ALL, "pt_BR");
 
-  // TODO: Try this one later
-  // setlocale(LC_ALL, "UTF-8");
-
   FILE *file_path = open_file(argv[1]);
 
-  BF_Node *buffer = NULL;
+  // TODO: Create a buffer
   store_file_buffer(&buffer, file_path);
 
   initscr();
@@ -52,51 +43,6 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-BF_Node *create_node(wchar_t file_char) {
-  BF_Node *new_node = (BF_Node *)malloc(sizeof(BF_Node));
-
-  if (new_node == NULL) {
-    printf("Something went wrong while reading the provided file!\nAborting...\n");
-    exit(1);
-  }
-
-  new_node->file_char = file_char;
-  new_node->next = NULL;
-  new_node->prev = NULL;
-
-  return new_node;
-}
-
-void insert_buffer(BF_Node **buffer, wchar_t file_char) {
-  BF_Node *new_node = create_node(file_char);  
-
-  if (*buffer == NULL) {
-    *buffer = new_node;
-    return;
-  }
-
-  BF_Node *temp = *buffer;
-
-  while (temp->next != NULL) {
-    temp = temp->next;
-  }
-
-  temp->next = new_node;
-  new_node->prev = temp;
-}
-
-void clean_buffer(BF_Node **buffer) {
-  BF_Node *temp = *buffer;
-
-  while (temp != NULL) {
-    BF_Node *next = temp->next;
-    free(temp);
-    temp = next;
-  }
-
-  *buffer = NULL;
-}
-
 FILE *open_file(char *argv) {
   FILE *file_path;
 
@@ -108,42 +54,12 @@ FILE *open_file(char *argv) {
   return file_path;
 }
 
-void close_file(FILE *file_path) {
-  fclose(file_path);
+void close_file(FILE *file_path) { fclose(file_path); }
+
+void store_file_buffer(Buffer buffer, FILE file_path) {
+  // TODO: transverse through a file and store it in a buffer
 }
 
-void store_file_buffer(BF_Node **buffer, FILE *file) {
-  wchar_t file_char;
-
-  do {
-    file_char = getc(file);
-
-    insert_buffer(buffer, file_char);
-  } while (file_char != EOF);
-}
-
-void display_buffer(BF_Node *buffer) {
-  move(0, 0);
-
-  int original_y_cu_pos = y_cursor_pos;
-  int original_x_cu_pos = x_cursor_pos;
-
-  BF_Node *temp = buffer;
-
-  while (temp->next != NULL) {
-    mvaddch(y_cursor_pos, x_cursor_pos, temp->file_char); 
-    x_cursor_pos++;
-
-    if (temp->file_char == '\n') {
-      y_cursor_pos++;   
-      x_cursor_pos = 0;
-    }
-    
-    temp = temp->next;
-  }
-
-  y_cursor_pos = original_y_cu_pos;
-  x_cursor_pos = original_x_cu_pos;
-
-  move(y_cursor_pos, x_cursor_pos);
+void display_buffer(Buffer buffer) {
+  // TODO: loop over the buffer vector and display it on the screen
 }
