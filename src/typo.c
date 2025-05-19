106828820ac140b5641e9a97710dc372d81ec294
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2025-05-18 21:42:48 -0300

    adding some funcitons, but still broken

--- src/typo.c
+++ src/typo.c
@@ -1,65 +1,129 @@
 #define NCURSES_WIDECHAR 1
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <curses.h>
 #include <locale.h>
 #include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <wchar.h>
 
 typedef struct Buffer {
   int current_cu_pointer;
+  int size;
   wchar_t *vect_buff;
 } Buffer;
 
+// file related operations
 FILE *open_file(char *argv);
-void close_file(FILE *file_path);
+void close_file(FILE *file);
 
-void store_file_buffer(Buffer buffer, FILE file_path);
+// buffer related operations
+int file_char_number(FILE *file);
+Buffer *create_buffer(FILE *file);
+void draw_buffer(Buffer *buffer);
 
-void display_buffer(Buffer buffer);
-
+// cursor position global variables
 int y_cursor_pos = 0;
 int x_cursor_pos = 0;
 
 int main(int argc, char *argv[]) {
   setlocale(LC_ALL, "pt_BR");
 
-  FILE *file_path = open_file(argv[1]);
+  FILE *file = open_file(argv[1]);
 
-  // TODO: Create a buffer
-  store_file_buffer(&buffer, file_path);
+  Buffer *buffer = create_buffer(file);
 
-  initscr();
-  cbreak();
-  noecho();
+	// debuging purposes
+	/* printf("%d", buffer->current_cu_pointer); */
+	/* printf("%d", buffer->size); */
+	/* printf("%c", buffer->vect_buff[8]); */
 
-  display_buffer(buffer);
+  /* initscr(); */
+  /* cbreak(); */
+  /* noecho(); */
+  /**/
+  /* draw_buffer(buffer); */
+  /* getch(); */
 
-  getch();
-  endwin();
-  close_file(file_path);
+  close_file(file);
 
   return 0;
 }
 
 FILE *open_file(char *argv) {
-  FILE *file_path;
+  FILE *file;
 
-  if ((file_path = fopen(argv, "r")) == NULL) {
-    printf("Não foi posspivel abrir o arquivo!\n");
+  if ((file = fopen(argv, "r")) == NULL) {
+    printf("couldn't open file\nAborting the program...\n");
     exit(1);
   }
 
-  return file_path;
+  return file;
 }
 
-void close_file(FILE *file_path) { fclose(file_path); }
+void close_file(FILE *file) { fclose(file); }
 
-void store_file_buffer(Buffer buffer, FILE file_path) {
-  // TODO: transverse through a file and store it in a buffer
+int file_char_number(FILE *file) {
+  int char_number = 0;
+  wchar_t file_char;
+
+  do {
+    file_char = getc(file);
+    char_number++;
+  } while (file_char != EOF);
+
+  char_number -= 2; // necessary because of the basic stream pointers to the
+                    // beginning/end of the file
+
+  return char_number;
 }
 
-void display_buffer(Buffer buffer) {
-  // TODO: loop over the buffer vector and display it on the screen
+// FIXME: broke after becoming a pointer
+Buffer *create_buffer(FILE *file) {
+  int char_number = file_char_number(file);
+  wchar_t file_char;
+
+  // initializing
+  Buffer *buffer;
+  buffer->current_cu_pointer = 0;
+  buffer->size = char_number;
+  buffer->vect_buff = calloc(char_number, sizeof(wchar_t));
+
+  rewind(file); // reseting file pointer
+
+  for (int i = 0; i <= char_number; i++) {
+    file_char = getc(file);
+		printf("%c", file_char);
+    buffer->vect_buff[i] = file_char;
+  }
+
+	// debuging purposes
+  /* for (int i = 0; i <= char_number; i++) { */
+  /* printf("%c", buffer->vect_buff[i]); */
+  /* } */
+
+  return buffer;
+}
+void draw_buffer(Buffer *buffer) {
+  // TODO: Implement
+  clear(); // TODO: extract this one to a funciton like start_game()
+  move(0, 0);
+
+  int x_pos = 0;
+  int y_pos = 0;
+
+  Buffer *temp = buffer;
+
+  /* for (int i = 0; i <= buffer->size; i++) { */
+  /*   mvaddch(y_pos, x_pos, buffer->vect_buff[i]); */
+  /**/
+  /*   // incrementing position */
+  /*   x_pos++; */
+  /*   if (buffer->vect_buff[i] == '\n') { */
+  /*     y_pos++; */
+  /*     x_pos = 0; */
+  /*   } */
+  /* } */
+
+  move(y_cursor_pos, x_cursor_pos); // going back to the original position
 }
